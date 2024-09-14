6f09dec3222e310b5fe73f82a85b9d8f93db82d3
Luiz Fernando Ferreira <luiz_f96@hotmail.com>
2024-09-14 12:55:29 -0300

    Add jest config and App test

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    clearMocks: true,
+    roots: ['<rootDir>/src'],
+    preset: 'ts-jest',
+    testEnvironment: 'jest-environment-jsdom',
+    setupFilesAfterEnv: ['<rootDir>/src/config/jest.setup.ts'],
+    moduleDirectories: ['node_modules', 'src'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+  };
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-const App: React.FC = () => {
-  return <h1>Hello, React with TypeScript!</h1>;
-};
+import App from './App';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
