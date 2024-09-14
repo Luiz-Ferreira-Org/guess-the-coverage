bc787c9aa8a0c7aca30e46ec59435bddd3d8ea38
Luiz Fernando Ferreira <luiz_f96@hotmail.com>
2024-09-14 18:09:43 -0300

    Add sonar properties and build file

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-    clearMocks: true,
-    roots: ['<rootDir>/src'],
-    preset: 'ts-jest',
-    testEnvironment: 'jest-environment-jsdom',
-    setupFilesAfterEnv: ['<rootDir>/src/config/jest.setup.ts'],
-    moduleDirectories: ['node_modules', 'src'],
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
-  };
+  clearMocks: true,
+  roots: ["<rootDir>/src"],
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/src/config/jest.setup.ts"],
+  moduleDirectories: ["node_modules", "src"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)"],
+  collectCoverage: true,
+  coverageReporters: ["lcov", "text"],
+  coverageDirectory: "coverage",
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-const App: React.FC = () => {const someValue: string = 'Hello, React with TypeScript!';someFuction(someValue);return (<h1>Hello, React with TypeScript!</h1>);const someValueNotCovered: string = 'Hello, I am not covered!';};
+const App: React.FC = () => { const someValue: string = "Hello, React with TypeScript!"; someFuction(someValue); return <h1>Hello, React with TypeScript!</h1>; const someValueNotCovered: string = "Hello, I am not covered!";};
 
-const someFuction = (value: string) => {console.log(`Hello, React with TypeScript! + ${value}`);};
+const someFuction = (value: string) => { console.log(`Hello, React with TypeScript! + ${value}`);};
 
 export default App;
