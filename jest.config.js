ba73faf399feb572627bdfa069a8301fa564580a
Luiz Fernando Ferreira <luiz_f96@hotmail.com>
2024-10-06 21:07:18 -0300

    Add global coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -10,4 +10,12 @@ module.exports = {
   collectCoverage: true,
   coverageReporters: ["lcov", "text"],
   coverageDirectory: "coverage",
+  coverageThreshold: {
+    global: {
+      statements: 95,
+      branches: 95,
+      functions: 95,
+      lines: 95,
+    },
+  },
 };
